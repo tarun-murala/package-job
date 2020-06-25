f2a541a188e99ccefc923e1042039ef16d6307ed
Tarun Murala <Tarun.Murala@servicenow.com>
2020-06-24 21:27:58 -0700

    Updated UITests.java

---
+++ src/test/java/com/tarun/UITest4.java
@@ -0,0 +1,37 @@
+package com.tarun;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class UITest4 {
+	
+	private FirefoxDriver driver;		
+	
+    @Test				
+	public void testServiceNow() {	
+		driver.get("http://servicenow.com");  
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(true); 		
+	}	
+	
+    @BeforeTest
+	public void beforeTest() {
+    		File pathToBinary = new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+    		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+    		FirefoxProfile firefoxProfile = new FirefoxProfile();
+    		driver = new FirefoxDriver(ffBinary,firefoxProfile);  
+	}		
+	
+    @AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}	
+}
